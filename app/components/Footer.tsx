c95b58f92f0bdc72cbd3fced8b57b23e52007c45
jawwadchoudhury <jawwadc@outlook.com>
2024-04-07 12:46:43 +0100

    Added GNU license

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,5 +1,6 @@
 //import './css/Navbar.css'
 import { Prompt } from 'next/font/google'
+import Link from 'next/link'
 
 const prompt = Prompt({
   subsets: ['latin'], 
@@ -9,7 +10,7 @@ const prompt = Prompt({
 export default function Navbar() {
   return (
     <div className="bg-[#333] w-full bottom-0 left-0 pl-[10px] h-[10vh] flex select-none">
-      <h1 className='text-white opacity-[60%] mt-auto mb-auto'><p className={prompt.className}>© 2024 Jawwad Choudhury<br/>Made entirely with NextJS, TailwindCSS and hosted with Firebase</p></h1>
+      <h1 className='text-white mt-auto mb-auto opacity-60'><p className={prompt.className}><Link href='/license' target='_blank'>© 2024 Jawwad Choudhury</Link><br/>Made entirely with NextJS, TailwindCSS and hosted with Vercel - all open source on <Link href="https://github.com/jawwadchoudhury/jawwadchoudhury" className='underline'>Github</Link>.</p></h1>
     </div>
   )
 }
